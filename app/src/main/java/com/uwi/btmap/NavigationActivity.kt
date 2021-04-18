9e24db53b8675e11cdfd43547470c9a550484e6f
Joshua Alkins <joshua_alkins@hotmail.com>
2021-04-18 21:14:42 +0800

    added basic mapview implementation to navigation activity

--- app/src/main/java/com/uwi/btmap/NavigationActivity.kt
+++ app/src/main/java/com/uwi/btmap/NavigationActivity.kt
@@ -1,11 +1,122 @@
 package com.uwi.btmap
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.LocationComponentOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.navigation.core.MapboxNavigation
 
-class NavigationActivity : AppCompatActivity() {
+class NavigationActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
+    private lateinit var map: MapView
+    private lateinit var mapView: MapView
+    private lateinit var mapboxMap: MapboxMap
+    private lateinit var mapboxNavigation: MapboxNavigation
+
+    private var permissionsManager: PermissionsManager = PermissionsManager(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+        setupMapView(savedInstanceState)
+    }
+
+    private fun setupMapView(savedInstanceState: Bundle?){
+        this.mapView = findViewById(R.id.mapView)
+        this.mapView?.onCreate(savedInstanceState)
+        this.mapView?.getMapAsync(this)
+    }
+
+    private fun setupNavigationObject(){
+        val mapboxNavigationOptions = MapboxNavigation
+            .defaultNavigationOptionsBuilder(this, getString(R.string.mapbox_access_token))
+            .build()
+
+        this.mapboxNavigation = MapboxNavigation(mapboxNavigationOptions)
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap) {
+        mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+            this.mapboxMap = mapboxMap
+//            show user location
+            enableLocationComponent(it)
+//
+//            setupMapIcons(it)
+//
+//            setupRouteLayer(it)
+//            setupLocationMarkerLayers(it)
+//
+//            mapboxMap.addOnMapLongClickListener(this)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableLocationComponent(loadedMapStyle: Style){
+        mapboxMap.getStyle {
+            if(PermissionsManager.areLocationPermissionsGranted(this)){
+                val customLocationComponentOptions = LocationComponentOptions.builder(this)
+                    .trackingGesturesManagement(true)
+                    .accuracyColor(ContextCompat.getColor(this,R.color.mapbox_blue))
+                    .build()
+
+                val locationComponentActivationOptions =
+                    LocationComponentActivationOptions.builder(this, loadedMapStyle)
+                        .locationComponentOptions(customLocationComponentOptions)
+                        .build()
+
+                mapboxMap.locationComponent.apply {
+                    activateLocationComponent(locationComponentActivationOptions)
+                    isLocationComponentEnabled = true
+                    cameraMode = CameraMode.TRACKING
+                    renderMode = RenderMode.COMPASS
+
+                    val lat = mapboxMap.locationComponent.lastKnownLocation?.latitude
+                    val lng = mapboxMap.locationComponent.lastKnownLocation?.longitude
+
+                    val position = CameraPosition.Builder()
+                        .zoom(12.0)
+                        .tilt(0.0)
+                        .target(LatLng(lat!!,lng!!))
+                        .build()
+
+                    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position))
+                }
+            }else{
+                permissionsManager = PermissionsManager(this)
+                permissionsManager.requestLocationPermissions(this)
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, "We need location services >:/", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if(granted){
+            enableLocationComponent(mapboxMap.style!!)
+        }else{
+            Toast.makeText(this, "permissions not granted", Toast.LENGTH_LONG).show()
+            finish()
+        }
     }
 }
